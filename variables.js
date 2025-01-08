5a9b01be215c23db1fe57cddeb083fda7c9fc885
Gangakhedkar Sriharsh <119785597+sriharsh43@users.noreply.github.com>
2025-01-08 09:44:10 +0000

    Javascript files

---
+++ variables.js
@@ -0,0 +1,9 @@
+const accountID = 133554
+let emailID = "sriharsh543@gmail.com"
+var accountPass = "23345" //var is a block scoped variable
+let accountCity = "Hyderabad"
+// console.log(emailID)
+console.log(accountCity)
+// console.table([accountID,emailID,accountPass,accountCity])
+emailID = "xsdf.@gmail.com"
+console.table([accountID,emailID,accountPass,accountCity])
