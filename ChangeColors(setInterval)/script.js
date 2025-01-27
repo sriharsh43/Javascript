fb547b026bdca58563c38955d2762a9958e3c8e2
Gangakhedkar Sriharsh <119785597+sriharsh43@users.noreply.github.com>
2025-01-27 10:19:42 +0000

    changeColors

---
+++ ChangeColors(setInterval)/script.js
@@ -0,0 +1,28 @@
+//Generate the random color
+const randomColor = function () {
+    const hex = '0123456789ABCDEF' //hex values from 0-16
+    let color = '#' //append the color to it
+    for (let i = 0; i <6; i++) {
+       color += hex[Math.floor(Math.random() * 16)]    
+    }
+    return color
+}
+let intervalID;
+const startChangingColor = function () {
+    if (!intervalID) {
+        intervalID = setInterval(changeBGColor,500)
+    }
+    function changeBGColor() {
+        document.body.style.backgroundColor = randomColor()
+    }
+}
+
+const stopChangingColor = function () {
+    clearInterval(intervalID)
+    intervalID = null
+    
+}
+document.getElementById('start').addEventListener('click',startChangingColor)
+
+
+document.getElementById('stop').addEventListener('click',stopChangingColor)
